05a778d479e6d4c6aaa26120721828dcea6889f9
Stepan Anchugov <kixxx1@gmail.com>
2014-12-15 15:42:41 +0500

    Fix command doc

--- src/SpiderSmoke/Command/CollectUrlsCommand.php
+++ src/SpiderSmoke/Command/CollectUrlsCommand.php
@@ -11,8 +11,8 @@ use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
 class CollectUrlsCommand extends Command
 {
-	
-	protected function configure()
+    
+    protected function configure()
     {   
         $start = 0;
         $stop = 100;
@@ -23,71 +23,61 @@ class CollectUrlsCommand extends Command
                   new InputOption('domain', 'd', InputOption::VALUE_REQUIRED, 'Domain to parse URLs from'),
             ))
              ->setHelp(<<<EOT
-Display the fibonacci numbers between a range of numbers given as parameters
-
-Usage:
-
-<info>php console.php phpmaster:fibonacci 2 18 <env></info>
-
-You can also specify just a number and by default the start number will be 0
-<info>php console.php phpmaster:fibonacci 18 <env></info>
-
-If you don't specify a start and a stop number it will set by default [0,100]
-<info>php console.php phpmaster:fibonacci<env></info>
+Crawls all accessible URLs on the given domain, and checks their HTTP return codes
 EOT
 );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-    	$domain = $input->getOption('domain');
+        $domain = $input->getOption('domain');
 
-		$crawler = new \Arachnid\Crawler(
-			$domain, 
-			3
-		);
+        $crawler = new \Arachnid\Crawler(
+            $domain, 
+            3
+        );
 
-		$output->writeln(sprintf('<info>Crawling %s...</info>', $domain));
-		$crawler->traverse();
-		$links = $crawler->getLinks();
+        $output->writeln(sprintf('<info>Crawling %s...</info>', $domain));
+        $crawler->traverse();
+        $links = $crawler->getLinks();
 
-		$output->writeln(sprintf('Collected <comment>%s</comment> URLs', count($links)));
+        $output->writeln(sprintf('Collected <comment>%s</comment> URLs', count($links)));
 
-		$table = $this->getHelper('table');
-		$table->setHeaders(array('Status', 'No. of URLs'));
-		$counts = array();
-		$results = array();
+        $table = $this->getHelper('table');
+        $table->setHeaders(array('Status', 'No. of URLs'));
+        $counts = array();
+        $results = array();
 
-		foreach ($links as $url => $info) {
-			if (!array_key_exists('status_code', $info)) {
-				continue;
-			}
+        foreach ($links as $url => $info) {
+            if (!array_key_exists('status_code', $info)) {
+                continue;
+            }
 
-			if (!array_key_exists($info['status_code'], $counts)) {
-				$counts[$info['status_code']] = 1;
-			} else {
-				$counts[$info['status_code']]++;
-			}
+            if (!array_key_exists($info['status_code'], $counts)) {
+                $counts[$info['status_code']] = 1;
+            } else {
+                $counts[$info['status_code']]++;
+            }
 
-			if ($info['status_code'] !== 200) {
-				if (!array_key_exists($info['status_code'], $results)) {
-					$results[$info['status_code']] = array();
-				}
+            if ($info['status_code'] !== 200) {
+                if (!array_key_exists($info['status_code'], $results)) {
+                    $results[$info['status_code']] = array();
+                }
 
-				$results[$info['status_code']] []= $info;
-			}
-			
-		}
+                $results[$info['status_code']] []= $info;
+            }
+            
+        }
 
-		$rows = array();
+        $rows = array();
 
-		foreach ($counts as $key => $value) {
-			$rows[]= array($key, $value);
-		}
+        foreach ($counts as $key => $value) {
+            $rows[]= array($key, $value);
+        }
 
-		$table->setRows($rows);
+        $table->setRows($rows);
 
-		$table->render($output);
+        $table->render($output);
     }
 
 }
